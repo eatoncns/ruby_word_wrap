b0af83cb57befe1f5d98cd5fe5c33726d37264b9
Philip Meier <philip.meier06@googlemail.com>
2017-07-18 17:20:03 +0100

    Refactor to remove duplication

--- kata.rb
+++ kata.rb
@@ -1,11 +1,19 @@
+def split_line(string, split_position, offset, line_length)
+  line_begin = 0
+  line_end = -1 
+  string[line_begin..split_position-1] << "\n" << word_wrap(string[split_position+offset..line_end], line_length)
+end
+
 def word_wrap(string, line_length)
   if string.length <= line_length
     return string
   end
   space = string.index(" ")
+  split_offset = 0
   if space.nil?
-    string[0..line_length-1] << "\n" << word_wrap(string[line_length..-1], line_length)
+    split_line(string, line_length, split_offset, line_length)
   else
-    string[0..space-1] << "\n" << word_wrap(string[space+1..-1], line_length) 
+    split_offset = 1
+    split_line(string, space, split_offset, line_length)
   end
 end
