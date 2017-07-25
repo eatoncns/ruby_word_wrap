e1dd9a92d792cffee61a598d59dad5c0434dc0a0
Philip Meier <philip.meier06@googlemail.com>
2017-07-25 10:02:21 +0100

    Update with feedback from mentors
    Updates:
     - Remove should in test descriptions
     - Constants should be screaming caps
     - Make RSpec.describe explicit
     - Fix some typos

--- kata.rb
+++ kata.rb
@@ -1,9 +1,9 @@
-StringBegin = 0
-StringEnd = -1
-OffsetByOne = 1
+STRING_BEGIN = 0
+STRING_END = -1
+OFFSET_BY_ONE = 1
 
 def split_line(string, split_position)
-  string[StringBegin..split_position-OffsetByOne] << "\n"
+  string[STRING_BEGIN..split_position-OFFSET_BY_ONE] << "\n"
 end
 
 def word_wrap(string, line_length)
@@ -12,8 +12,10 @@ def word_wrap(string, line_length)
   end
   last_space_in_line = string.rindex(" ", line_length)
   if last_space_in_line.nil?
-    split_line(string, line_length) << word_wrap(string[line_length..StringEnd], line_length)
+    split_line(string, line_length) <<
+      word_wrap(string[line_length..STRING_END], line_length)
   else
-    split_line(string, last_space_in_line) << word_wrap(string[last_space_in_line + OffsetByOne..StringEnd], line_length)
+    split_line(string, last_space_in_line) <<
+      word_wrap(string[last_space_in_line + OFFSET_BY_ONE..STRING_END], line_length)
   end
 end
--- spec.rb
+++ spec.rb
@@ -1,41 +1,41 @@
 require_relative 'kata'
 
-describe "word_wrap" do
+RSpec.describe "word_wrap" do
 
   context "given empty input" do
-    it "should return empty string" do
+    it "returns empty string" do
       expect(word_wrap("", 1)).to eq ""
     end
   end
 
   context "given single word input" do
-    it "should return word when smaller than line length" do
+    it "returns word when smaller than line length" do
       expect(word_wrap("test", 5)).to eq "test"
     end
 
-    it "should split word when longer than line length" do
+    it "splits word when longer than line length" do
       expect(word_wrap("test", 2)).to eq "te\nst"
     end
 
-    it "should perform mulitple splits when necessary" do
+    it "performs multiple splits when necessary" do
       expect(word_wrap("tested", 2)).to eq "te\nst\ned"
     end
   end
 
   context "given multiple word input" do
-    it "should split on word boundary" do
+    it "splits on word boundary" do
       expect(word_wrap("test test", 5)).to eq "test\ntest"
     end
 
-    it "avoid splitting words where possible" do
+    it "avoids splitting words where possible" do
       expect(word_wrap("test test", 6)).to eq "test\ntest"
     end
 
-    it "should split after word boundary" do
+    it "splits after word boundary" do
       expect(word_wrap("test test", 4)).to eq "test\ntest"
     end
 
-    it "should handle multiple words per line" do
+    it "handles multiple words per line" do
       expect(word_wrap("a lot of words for a single line", 10)).to eq "a lot of\nwords for\na single\nline"
     end
   end
